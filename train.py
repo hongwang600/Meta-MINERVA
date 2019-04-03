23c020b08cdade768175c18ea412e4d162db7569
anan <anan@169-231-93-80.wireless.ucsb.edu>
2019-04-03 13:15:37 -0700

    use lstm as encoder

--- train.py
+++ train.py
@@ -134,12 +134,10 @@ def train(args):
 
     task_results = np.zeros(6)
     count = 0
-    #to_train_data = concated_meta_train_data + concated_train_data
-    #to_dev_data = concated_meta_dev_data + concated_dev_data
     to_train_data = concated_train_data
     to_dev_data = concated_dev_data
-    #to_dev_data = to_train_data[:2000]
-    #to_train_data = to_train_data[:-2000]
+    #to_train_data = concated_meta_train_data + concated_train_data
+    #to_dev_data = concated_meta_dev_data + concated_dev_data
     #to_train_data = concated_meta_train_data
     #to_dev_data = concated_meta_dev_data
     #to_train_data = meta_train_data
@@ -147,7 +145,9 @@ def train(args):
     #to_train_data.update(train_data)
     #to_dev_data = concated_meta_dev_data
     random.shuffle(to_train_data)
-    random.shuffle(to_dev_data)
+    #to_dev_data = to_train_data[:2000]
+    #to_train_data = to_train_data[:-2000]
+    #random.shuffle(to_dev_data)
     agent = train_on_dataset(to_train_data, to_dev_data, writer, args)
     for task_id, task in enumerate(meta_train_data):
         if len(meta_dev_data[task])>0:
